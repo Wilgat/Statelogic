36c0c2847af61159bd34203f95b74a61738940a0
Wong Chun Fai <cloudgen.wong@mmsolutions.io>
2025-03-14 14:54:03 +0800

    Add long description to pypi

--- build.sh
+++ build.sh
@@ -3,7 +3,9 @@ find . -type d -name __pycache__ -exec rm -rf {} \;
 find . -type d -name __pycache__ -exec rm -rf {} \;
 find . -type d -name statelogic.egg-info -exec rm -rf {} \;
 find . -type f -name "._*" -exec rm -rf {} \;
-find . -type f -name "._*" -exec rm -rf {} \;
 rm -rf build
 rm -rf dist
+find . -type f -name "._*" -exec rm -rf {} \;
+python3 setup.py sdist bdist_wheel
+find . -type f -name "._*" -exec rm -rf {} \;
 python3 setup.py sdist bdist_wheel
